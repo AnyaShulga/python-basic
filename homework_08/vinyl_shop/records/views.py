88068ce2df7cc5dd7c84a30b0c288af878fad3a9
AnyaShulga <anya.shulga93@gmail.com>
2022-11-13 17:28:57 +0300

    View functions description

---
+++ homework_08/vinyl_shop/records/views.py
@@ -0,0 +1,30 @@
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, DeleteView
+
+from .models import Record
+
+
+class RecordListView(ListView):
+    template_name = "records/record_list.html"
+    queryset = (
+        Record
+        .objects
+        .order_by("artist")
+        .all()
+    )
+
+
+class RecordDetailView(DetailView):
+    template_name = "records/record_details.html"
+    queryset = (
+        Record
+        .objects
+        .select_related("record_label")
+        .prefetch_related("genre")
+    )
+
+
+class RecordDeleteView(DeleteView):
+    template_name = "records/record_delete.html"
+    model = Record
+    success_url = reverse_lazy("records:list")
